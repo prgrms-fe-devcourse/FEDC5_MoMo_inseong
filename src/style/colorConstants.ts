52471aadad2a5309aace90bd34d1e81c5cd7e99c
yejinleee <yejin9487@gmail.com>
2023-12-28 11:35:14 +0900

    Feat: 공통 ui 컴포넌트 Button 구현

---
+++ src/style/colorConstants.ts
@@ -0,0 +1,10 @@
+export const PRIMARY_BLUE = '#228BB4';
+export const PRIMARY_BLUE_HOVER = '#1E779A';
+export const SECONDARY_NAVY = '#364954';
+export const SECONDARY_NAVY_HOVER = '#232E34';
+export const BRIGHT_GREY = '#F8F9FA';
+export const LIGHT_GREY = '#EDEDED';
+export const GREY = '#B2B2B2';
+export const DARK_GREY = '#939393';
+export const BEIGE = '#EEEAE2';
+export const SEMI_WHITE = '#F8F9FA';
