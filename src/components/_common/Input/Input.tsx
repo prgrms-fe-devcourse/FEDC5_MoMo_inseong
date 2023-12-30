588405c590261a02b9573d68245901b68c3c3f51
km6293 <alstn7616@naver.com>
2023-12-30 15:50:06 +0900

    Refactor : fontsize 추가, width-태그 컨트롤, props 추가, height 동적 컨트롤, keyDown 이벤트 수정

--- src/components/_common/Input/Input.tsx
+++ src/components/_common/Input/Input.tsx
@@ -1,10 +1,11 @@
-import styled from "@emotion/styled";
-import { useState, KeyboardEvent } from "react";
-import InputUpload from "./InputUpload";
+import styled from '@emotion/styled';
+import { KeyboardEvent, useState } from 'react';
+import InputUpload from './InputUpload';
+import { LIGHT_GREY, PRIMARY_BLUE } from '@/style/colorConstants';
 
 interface InputProps {
-  width?: string;
-  height?: string;
+  fontSize?: number;
+  width?: string | number;
   placeholder: string;
   isTextarea?: boolean;
   hasTags?: boolean;
@@ -13,26 +14,29 @@ interface InputProps {
   image?: string;
 }
 
-interface IInputStyle{
-  width: string;
-  height: string;
+interface IInputStyle {
+  width: string | number;
+  hasTags: boolean;
 }
 
 export const Input = ({
-  width = '100%',
-  height = 'auto',
+  fontSize = 16,
+  width = 300,
   placeholder,
   isTextarea = false,
   hasTags = false,
   tags: initialTags = [],
   hasImage = false,
   image: initialImage,
+  ...props
 }: InputProps) => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
   const [tags, setTags] = useState(initialTags);
   const [image, setImage] = useState<string | null>(initialImage || null);
 
-  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleKeyUp = (
+    event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     if (event.key === 'Enter' && inputValue.trim() && hasTags) {
       setTags([...tags, inputValue.trim()]);
       setInputValue('');
@@ -57,38 +61,45 @@ export const Input = ({
   };
 
   return (
-    <StInputContainer width={width} height={height}>
+    <StInputContainer
+      width={width}
+      hasTags={hasTags}
+      {...props}>
       {isTextarea ? (
         <StTextArea
+          fontSize={fontSize}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
           placeholder={placeholder}
         />
       ) : (
         <StInputText
+          fontSize={fontSize}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
           placeholder={placeholder}
         />
       )}
 
       {/* TODO: 태그 컴포넌트 병합시 진행 */}
       {hasTags && (
-        <StTagsContainer>
+        <StTagsContainer hasTags={tags.length > 0}>
           {tags.map((tag, index) => (
             <StTag key={index}>
               {tag}
-              <StTagRemoveButton onClick={() => handleTagRemove(index)}>x</StTagRemoveButton>
+              <StTagRemoveButton onClick={() => handleTagRemove(index)}>
+                x
+              </StTagRemoveButton>
             </StTag>
           ))}
         </StTagsContainer>
       )}
 
       {/* TODO: Image 클릭 시 확장 어떻게 진행할지 */}
-      {hasImage && (
-        !image ? (
+      {hasImage &&
+        (!image ? (
           <StImageContainer>
             <StyledUpload>
               <InputUpload onChange={handleImageChange}>+</InputUpload>
@@ -96,13 +107,15 @@ export const Input = ({
           </StImageContainer>
         ) : (
           <StImageContainer>
-            <StImage style={{ backgroundImage: `url(${image})` }} onClick={handleImageZoom}>
-              <StImageDeleteButton onClick={handleImageRemove}>x</StImageDeleteButton>
+            <StImage
+              style={{ backgroundImage: `url(${image})` }}
+              onClick={handleImageZoom}>
+              <StImageDeleteButton onClick={handleImageRemove}>
+                x
+              </StImageDeleteButton>
             </StImage>
           </StImageContainer>
-        )
-      )}
-
+        ))}
     </StInputContainer>
   );
 };
@@ -112,49 +125,50 @@ const StInputContainer = styled.div<IInputStyle>`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  border: 1px solid #EDEDED;
+  position: relative;
+  border: 1px solid ${LIGHT_GREY};
   border-radius: 8px;
   min-height: 50px;
   padding: 15px 24px;
-  width: ${({ width }) => width};
-  height: ${({ height }) => height};
-  
+  width: ${({ width, hasTags }) =>
+    hasTags ? '100%' : typeof width === 'number' ? `${width}px` : width};
+
   &:focus-within {
-    border-color: #228BB4;
+    border-color: ${PRIMARY_BLUE};
   }
 `;
 
-const StInputText = styled.input`
+const StInputText = styled.input<{ fontSize: number }>`
   border: none;
   outline: none;
   width: 100%;
   height: 24px;
-  font-size: 16px;
+  font-size: ${({ fontSize }) => `${fontSize}px`};
 `;
 
-const StTextArea = styled.textarea`
+const StTextArea = styled.textarea<{ fontSize: number }>`
   border: none;
   outline: none;
   width: 100%;
   min-height: 24px;
-  font-size: 16px;
+  font-size: ${({ fontSize }) => `${fontSize}px`};
   resize: none;
-  overflow-y: scroll; 
+  overflow-y: scroll;
 
   &::-webkit-scrollbar {
     display: none;
   }
 `;
 
-const StTagsContainer = styled.div`
+const StTagsContainer = styled.div<{ hasTags: boolean }>`
   display: flex;
   flex-wrap: wrap;
-  padding-top: 10px;
+  padding-top: ${({ hasTags }) => (hasTags ? '10px' : '0')};
 `;
 
 // TODO: color 통일
 const StTag = styled.div`
-  background: #EEE;
+  background: #eee;
   margin-right: 10px;
   margin-bottom: 10px;
   padding: 4px 8px;
@@ -201,12 +215,12 @@ const StImageDeleteButton = styled.span`
 `;
 
 const StyledUpload = styled.div`
-  width: 50px; 
+  width: 50px;
   height: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  border: 1px solid #EDEDED;
+  border: 1px solid ${LIGHT_GREY};
   border-radius: 8px;
 `;
