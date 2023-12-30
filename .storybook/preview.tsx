7590bdb8ad22820a207c7cf3fd54f19e12414ddb
wdgWon <same666@naver.com>
2023-12-30 20:47:27 +0900

    Chore : 글로벌 스타일, 테마 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,12 @@
-import type { Preview } from "@storybook/react";
-import React from "react";
-import { GlobalReset } from "../src/style/GlobalReset";
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+import { GlobalReset } from '../src/style/GlobalReset';
+import { theme } from '../src/style/theme';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -14,10 +16,10 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <>
+      <ThemeProvider theme={theme}>
         <GlobalReset />
         <Story />
-      </>
+      </ThemeProvider>
     ),
   ],
 };
