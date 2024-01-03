d78eec42d563ba11f755a2859911fee25865cbd3
km6293 <alstn7616@naver.com>
2024-01-03 13:01:06 +0900

    Feat: 로그인, 회원가입 UI 작업

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,78 @@
+import styled from '@emotion/styled';
+import { theme } from '@/style/theme';
+import { Button } from '@common/Button/Button';
+import { Input } from '@common/Input/Input';
+
 export const LoginPage = () => {
-  return <></>;
+  return (
+    <StLoginContainer>
+      <StDescriptionContainer>LOGO TEXT</StDescriptionContainer>
+      <StVerticalLine />
+      <StLoginFormContainer>
+        <StFormTitle>로그인</StFormTitle>
+        <StInputContainer>
+          <Input placeholder="이메일" />
+        </StInputContainer>
+        <StInputContainer>
+          <Input
+            placeholder="비밀번호"
+            type="password"
+          />
+        </StInputContainer>
+        <Button
+          label="확인"
+          type="submit"
+        />
+        <StSignupLink>회원가입</StSignupLink>
+      </StLoginFormContainer>
+    </StLoginContainer>
+  );
 };
+
+const StLoginContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StDescriptionContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StVerticalLine = styled.div`
+  width: 1px;
+  background-color: ${theme.colors.grey.light};
+  height: 90%;
+`;
+
+const StLoginFormContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: 'blue';
+  gap: 25px;
+`;
+
+const StFormTitle = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const StInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+`;
+
+const StSignupLink = styled.div`
+  color: ${theme.colors.grey.default};
+  cursor: pointer;
+`;
--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -1,3 +1,93 @@
-export const SignupPage = () => {
-  return <></>;
+import styled from '@emotion/styled';
+import { theme } from '@/style/theme';
+import { Button } from '@common/Button/Button';
+import { Input } from '@common/Input/Input';
+
+export const SignUpPage = () => {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+  // const [nickname, setNickname] = useState('');
+  // const [isValid, setIsValid] = useState(false);
+
+  // useEffect(() => {
+  //   const handleValidateEmail = (email: string) =>
+  //     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  //   const handleValidatePassword = (password: string) => password.length >= 6;
+  //   const isPasswordMatching = password === confirmPassword;
+  //   const hasNickname = nickname.trim().length > 0;
+
+  //   setIsValid(
+  //     handleValidateEmail(email) &&
+  //       handleValidatePassword(password) &&
+  //       isPasswordMatching &&
+  //       hasNickname,
+  //   );
+  // }, [email, password, confirmPassword, nickname]);
+
+  // const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setEmail(e.target.value);
+  // };
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  // };
+
+  return (
+    <StSignUpContainer>
+      <StDescriptionContainer>LOGO TEXT</StDescriptionContainer>
+      <StVerticalLine />
+      <StSignUpFormContainer>
+        <StFormTitle>회원가입</StFormTitle>
+
+        <Input placeholder="이메일" />
+        <Input
+          placeholder="비밀번호"
+          type="password"
+        />
+        <Input
+          placeholder="비밀번호 재확인"
+          type="password"
+        />
+        <Input placeholder="닉네임" />
+        <Button label="가입" />
+      </StSignUpFormContainer>
+    </StSignUpContainer>
+  );
 };
+
+const StSignUpContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StDescriptionContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StVerticalLine = styled.div`
+  width: 1px;
+  background-color: ${theme.colors.grey.light};
+  height: 90%;
+`;
+
+const StSignUpFormContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: 'blue';
+  gap: 25px;
+`;
+
+const StFormTitle = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
