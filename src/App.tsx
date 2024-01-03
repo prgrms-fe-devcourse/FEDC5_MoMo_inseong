20aa97d21c5ff43874f02b72e1cf56b39e8b125c
타래 <85001878+meowTarae@users.noreply.github.com>
2024-01-03 16:58:49 +0900

    Chore: App 파일 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 import { GlobalReset } from './style/GlobalReset';
 import { theme } from './style/theme';
@@ -7,18 +6,8 @@ import { theme } from './style/theme';
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <StContainer>
         <GlobalReset />
         <Outlet />
-      </StContainer>
     </ThemeProvider>
   );
 };
-
-const StContainer = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  box-sizing: border-box;
-  /* background-color: #23272e8a; */
-  /* color: snow; */
-`;
