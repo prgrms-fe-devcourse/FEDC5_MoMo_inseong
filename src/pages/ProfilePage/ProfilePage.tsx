273142061ece86a4c4827e7ea9d05615a7ed4c64
km6293 <alstn7616@naver.com>
2024-01-08 13:54:43 +0900

    feat: myPage 헤더, 탭 작성

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,56 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { ProfileTab } from './ProfileTab';
+import { StSideMarginWrapper } from '@/style/StSideMarginWrapper';
+import { Button } from '@common/Button/Button';
+import { Profile } from '@common/Profile/Profile';
+
 export const ProfilePage = () => {
-  return <></>;
+  const [pageNumber, setPageNumber] = useState(1);
+
+  return (
+    <StSideMarginWrapper>
+      <StProfileActionsContainer>
+        <Profile
+          image={'https://picsum.photos/200'}
+          fullName={'name'}
+          _id={'testId'}
+          status={'Profile'}
+          fontSize={16}
+        />
+        <StButtonsContainer>
+          <Button label="프로필 수정" />
+          <Button label="비밀번호 변경" />
+        </StButtonsContainer>
+      </StProfileActionsContainer>
+      <StProfileContainer>
+        <ProfileTab
+          pageNumber={pageNumber}
+          handleCreatePostClick={() => setPageNumber(1)}
+          handleAttendedPostClick={() => setPageNumber(2)}
+          handleInterestedPostClick={() => setPageNumber(3)}
+        />
+      </StProfileContainer>
+    </StSideMarginWrapper>
+  );
 };
+
+const StProfileContainer = styled.div`
+  padding: 32px;
+`;
+
+const StProfileActionsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 32px;
+`;
+
+const StButtonsContainer = styled.div`
+  display: flex;
+  gap: 10px;
+
+  .button {
+    margin: 0 5px;
+  }
+`;
