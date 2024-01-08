12f149f2d710ed2e5c0aa028adad1d4ad7f11029
타래 <85001878+meowTarae@users.noreply.github.com>
2024-01-08 16:30:21 +0900

    Chore: merge 충돌 해결

--- src/components/_common/Button/Button.tsx
+++ src/components/_common/Button/Button.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-// import useAxios from '@/api/useAxios';
+import useAxios from '@/api/useAxios';
 import {
   BRIGHT_GREY,
   PRIMARY_BLUE,
@@ -25,16 +25,16 @@ export const Button = ({
   handleButtonClick,
   ...props
 }: ButtonProps) => {
-  // const { response, loading, error, sendData } = useAxios(
-  //   {
-  //     method: 'get',
-  //     url: `/users/get-users?offset=0&limit=10`,
-  //   },
-  //   '',
-  // );
-  // if (response) {
-  //   console.log('응답 data', response.data);
-  // }
+  const { response, loading, error, sendData } = useAxios(
+    {
+      method: 'get',
+      url: `/users/get-users?offset=0&limit=10`,
+    },
+    '',
+  );
+  if (response) {
+    console.log('응답 data', response.data);
+  }
   const buttonStyle = {
     width: `${width}px`,
     height: `${height}px`,
