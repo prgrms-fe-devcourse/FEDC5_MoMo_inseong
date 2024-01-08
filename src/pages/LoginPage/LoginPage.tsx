2ba806165859500a0f3b4916006b75d975ff26dc
km6293 <alstn7616@naver.com>
2024-01-08 12:49:14 +0900

    Feat: input 변경

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,35 +1,98 @@
 import styled from '@emotion/styled';
+import { FormEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from '@/_redux/hooks';
+import { getUserInfo } from '@/_redux/slices/userSlice';
+import { IUser } from '@/api/_types/apiModels';
+import { postApi } from '@/api/apis';
+import { StSideMarginWrapper } from '@/style/StSideMarginWrapper';
 import { theme } from '@/style/theme';
+import { getItem, setItem } from '@/utils/storage';
 import { Button } from '@common/Button/Button';
-import { Input } from '@common/Input/Input';
+import { InputCompound } from '@common/Input/InputCompound';
 
 export const LoginPage = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('');
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const dispatch = useDispatch();
+  const handleLogin = async (
+    e: FormEvent<HTMLDivElement> | KeyboardEvent<HTMLDivElement>,
+  ) => {
+    e.preventDefault();
+    await postApi<{
+      user: IUser;
+      token: string;
+    }>('/login', { email, password })
+      .then((res) => {
+        setItem('JWT', res.data.token);
+        void dispatch(getUserInfo());
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoginError('로그인 정보가 잘못 되었습니다.');
+        emailRef.current?.focus();
+      });
+  };
+
+  useEffect(() => {
+    if (getItem('JWT')) {
+      navigate('/');
+    }
+  }, [navigate]);
+  const handleOnKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      void handleLogin(e);
+    }
+  };
+
   return (
-    <StLoginContainer>
-      <StDescriptionContainer>LOGO TEXT</StDescriptionContainer>
-      <StVerticalLine />
-      <StLoginFormContainer>
-        <StFormTitle>로그인</StFormTitle>
-        <StInputContainer>
-          <Input placeholder="이메일" />
-        </StInputContainer>
-        <StInputContainer>
-          <Input
-            placeholder="비밀번호"
-            type="password"
+    <StSideMarginWrapper>
+      <StLoginContainer>
+        <StDescriptionContainer>LOGO TEXT</StDescriptionContainer>
+        <StVerticalLine />
+        <StLoginFormContainer>
+          <StFormTitle>로그인</StFormTitle>
+          <StInputText>
+            <InputCompound style={{ width: '300px' }}>
+              <InputCompound.Text
+                placeholder="이메일"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                ref={emailRef}
+                onKeyUp={handleOnKeyUp}
+              />
+            </InputCompound>
+            {loginError}
+          </StInputText>
+          <StInputText>
+            <InputCompound style={{ width: '300px' }}>
+              <InputCompound.Text
+                placeholder="비밀번호"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                ref={passwordRef}
+                onKeyUp={handleOnKeyUp}
+              />
+            </InputCompound>
+          </StInputText>
+          <Button
+            label={'확인'}
+            type="submit"
+            onClick={handleLogin}
           />
-        </StInputContainer>
-        <Button
-          label="확인"
-          type="submit"
-        />
-        <StSignupLink onClick={() => navigate('/signup')}>
-          회원가입
-        </StSignupLink>
-      </StLoginFormContainer>
-    </StLoginContainer>
+          <StSignupLink onClick={() => navigate('/signUp')}>
+            회원가입
+          </StSignupLink>
+        </StLoginFormContainer>
+      </StLoginContainer>
+    </StSideMarginWrapper>
   );
 };
 
@@ -59,8 +122,6 @@ const StLoginFormContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  background-color: 'blue';
-  gap: 25px;
 `;
 
 const StFormTitle = styled.h1`
@@ -69,14 +130,15 @@ const StFormTitle = styled.h1`
   margin-bottom: 20px;
 `;
 
-const StInputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  position: relative;
-`;
-
 const StSignupLink = styled.div`
   color: ${theme.colors.grey.default};
   cursor: pointer;
+  padding-top: 30px;
+`;
+
+const StInputText = styled.div`
+  height: 85px;
+  max-width: 300px;
+  font-size: 14px;
+  color: ${theme.colors.red};
 `;
