c88ca2feb95aa57ed4799cf97c14ad927b111e73
yejinleee <yejin9487@gmail.com>
2024-01-09 01:15:05 +0900

    Feat: useAxios, custom api요청 수정

--- src/pages/MainPage/CardsArea/MainArea.tsx
+++ src/pages/MainPage/CardsArea/MainArea.tsx
@@ -2,12 +2,19 @@ import styled from '@emotion/styled';
 import { useState } from 'react';
 import { ScheduledMain } from './ScheduledMain';
 import { WhenCards } from './WhenCards';
+// import { ILike } from '@/api/_types/apiModels';
+// import { postApiJWT } from '@/api/apis';
+// import useAxios from '@/api/useAxios';
 import { Tab } from '@common/Tab/Tab';
 
 export const MainArea = () => {
   const [selectedTab, setSelectedTab] = useState(0);
+  // const { response, error, isLoading } = useAxios<ILike>(() =>
+  //   postApiJWT('/likes/create', { postId: '65963987cd1f0813f5ab851a' }),
+  // );
   return (
     <div>
+      <button onClick={() => console.log('')}>post 테텥테테트스트</button>
       <StTabsWrapper>
         <Tab
           label="언제 모일래?"
