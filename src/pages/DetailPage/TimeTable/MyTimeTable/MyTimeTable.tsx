097bfb0389eb7731ea4cccb078092b4a7ba25941
wdgWon <same666@naver.com>
2024-01-08 13:03:43 +0900

    Feat : 타임테이블 로직 구현

--- src/pages/DetailPage/TimeTable/MyTimeTable/MyTimeTable.tsx
+++ src/pages/DetailPage/TimeTable/MyTimeTable/MyTimeTable.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { forwardRef } from 'react';
 import { IVote } from '../TimeTable';
 
+const days = ['일', '월', '화', '수', '목', '금', '토'];
 interface MyTimeTableProps {
   meetDate: string[];
   vote: IVote;
@@ -10,86 +11,238 @@ interface MyTimeTableProps {
 export const MyTimeTable = forwardRef<HTMLDivElement, MyTimeTableProps>(
   ({ meetDate, vote }, ref) => {
     return (
-      <StTable
-        ref={ref}
-        onClick={() => console.log(ref)}>
-        {meetDate.map((date, i) => (
-          <StTableColumn
-            key={date}
-            data-date={date}>
-            {Object.entries(vote[date]).map(([time, users], j) => (
-              <StCell
-                key={date + time}
-                data-col={i}
-                data-row={j}
-                data-time={time}
-                data-users={users.map(({ fullName }) => fullName).join(' ')}
-              />
-            ))}
-          </StTableColumn>
-        ))}
+      <StTable>
+        <StRowHeader>
+          {meetDate.map((format, i) => {
+            //FIXME: 함수로 리팩토링
+
+            const fullDate = new Date(format);
+            const day = fullDate.getDay();
+            const month = fullDate.getMonth() + 1;
+            const date = fullDate.getDate();
+
+            return (
+              <div key={i}>
+                <div
+                  style={{
+                    fontSize: '8px',
+                    fontWeight: '700',
+                  }}>{`${month} / ${date}`}</div>
+                <div>{days[day]}</div>
+              </div>
+            );
+          })}
+        </StRowHeader>
+        <StOuterWrapper>
+          <StScrollWrapper>
+            <StBody>
+              <StColumnHeader>
+                {
+                  //FIXME: 함수로 리팩토링
+                  Object.keys(Object.values(vote)[0]).map(
+                    (time, i) => i % 2 === 0 && <span key={i}>{time}</span>,
+                  )
+                }
+                <div style={{ paddingTop: '1px' }}></div>
+              </StColumnHeader>
+              <StCellContainer ref={ref}>
+                {meetDate.map((date, i) => (
+                  <StColumnCells
+                    key={date}
+                    data-date={date}>
+                    {Object.entries(vote[date]).map(([time, users], j) => (
+                      <StCell
+                        key={date + time}
+                        data-time={time}
+                        data-users={users
+                          .map(({ fullName }) => fullName)
+                          .join(' ')}
+                        data-col={j}
+                        data-row={i}
+                        onClick={(e) => console.log(e.target)}
+                      />
+                    ))}
+                  </StColumnCells>
+                ))}
+              </StCellContainer>
+            </StBody>
+          </StScrollWrapper>
+        </StOuterWrapper>
       </StTable>
     );
   },
 );
 
 const StTable = styled.div`
-  padding: 36px 16px 16px 36px;
-  display: flex;
-  flex-direction: row;
+  position: relative;
+  padding-top: 36px;
+  /* FIXME: 전역으로 관리 필요 */
+  max-height: 300px;
+  overflow-y: hidden;
 `;
 
-const StTableColumn = styled.div`
+const StRowHeader = styled.div`
+  position: absolute;
+  top: 0;
+  left: 36px;
+  right: 12px;
+  height: 36px;
+
+  font-family: ${({ theme }) => theme.fonts};
+  font-size: 10px;
+  font-weight: 500px;
+  text-align: center;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: end;
+`;
+
+const StBody = styled.div`
+  position: relative;
+  padding-left: 36px;
+`;
+
+const StColumnHeader = styled.div`
+  position: absolute;
+  top: -6px;
+  left: 0;
+  bottom: 6px;
+  width: 36px;
+
+  font-family: ${({ theme }) => theme.fonts};
+  font-size: 10px;
+  font-weight: 500px;
+  text-align: center;
+
   display: flex;
   flex-direction: column;
-  /* 
-  &:first-child :nth-child(2n + 2) {
-    &::before {
-      font-size: 12px;
-      position: absolute;
-      display: block;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      background-color: orange;
-    }
-    border-color: red;
-  } */
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StCellContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  user-select: none;
+`;
+
+const StColumnCells = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StOuterWrapper = styled.div`
+  position: relative;
+  max-height: 264px;
+
+  ::before,
+  ::after {
+    content: '';
+    position: absolute;
+    /* FIXME: 전역으로 관리 필요 */
+    left: 36px;
+    right: 6px;
+    height: 8px;
+    z-index: 1;
+  }
+
+  ::before {
+    top: 0;
+    background: linear-gradient(to bottom, white, transparent);
+  }
+
+  ::after {
+    bottom: 0;
+    background: linear-gradient(to top, white, transparent);
+  }
+`;
+
+const StScrollWrapper = styled.div`
+  position: relative;
+  max-height: 264px;
+  padding-right: 6px;
+  padding-top: 8px;
+
+  overflow-y: auto;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+
+  ${({ theme }) => theme.scrollBar.default}
+
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-clip: padding-box;
+    border: 1px 0 solid transparent;
+    background-color: transparent;
+  }
+
+  &:hover::-webkit-scrollbar-thumb {
+    background-color: #228bb4;
+  }
 `;
 
 const StCell = styled.div`
   position: relative;
-  background-color: rgb(255, 222, 222);
-  border: 1px solid rgb(82, 157, 255);
-  border-radius: 4px;
-  padding: 6px 16px;
-  cursor: pointer;
+  padding: 0 4px 4px 0;
 
-  /* :first-of-type::before {
-    content: 'gd';
-  } */
+  &::before {
+    content: '';
+    display: block;
+    background-color: rgb(255, 222, 222);
+    border-radius: 4px;
+    cursor: pointer;
 
-  /* :first::before {
-    content: 'gd';
+    /* 간격도 Cell에 포함되어야 함 */
+    padding: 6px 16px;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.beige};
+      box-shadow: 0 0 2px 1px olive;
+    }
   }
 
-  :first-child::before {
-    content: 'gd';
-  } */
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.beige};
-    border-color: whitesmoke;
-    box-shadow: 0 0 2px 1px olive;
+  &:last-child {
+    margin-bottom: 4px;
   }
 
   &.selected {
-    &:hover {
-      filter: brightness(120%);
-      border-color: ${({ theme }) => theme.colors.beige};
-      box-shadow: 0 0 2px 1px aquamarine;
+    &::before {
+      background-color: ${({ theme }) => theme.colors.grey.default};
+
+      &:hover {
+        filter: brightness(120%);
+        border-color: ${({ theme }) => theme.colors.beige};
+      }
     }
-    background-color: ${({ theme }) => theme.colors.primaryBlue.default};
+  }
+
+  &.selecting {
+    &::before {
+      background-color: ${({ theme }) => theme.colors.primaryBlue.default};
+
+      &:hover {
+        filter: brightness(120%);
+        border-color: ${({ theme }) => theme.colors.beige};
+      }
+    }
+  }
+
+  &.unselecting {
+    &::before {
+      background-color: rgb(255, 222, 222);
+
+      &:hover {
+        filter: brightness(120%);
+        border-color: ${({ theme }) => theme.colors.beige};
+      }
+    }
   }
 `;
--- src/pages/DetailPage/hooks/useVotingTimeTable.ts
+++ src/pages/DetailPage/hooks/useVotingTimeTable.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { IVote } from '../TimeTable/TimeTable';
 import { useDragArea } from '@/hooks/useDragArea';
 
@@ -9,161 +9,175 @@ interface IuseVotingTimeTable {
 
 export const useVotingTimeTable = ({ vote, userId }: IuseVotingTimeTable) => {
   const voteAreaRef = useRef<HTMLDivElement>(null);
-  const dragStartPoint = useRef([0, 0]);
+  const startDragPoint = useRef<number[]>([-1, -1]);
+  const prevDragPoint = useRef<number[]>([-1, -1]);
 
-  const prevVotes = useRef<boolean[][]>([[]]); //FIXME: 이전 투표값 받아와야 됨
+  // 투표 진행전 내 투표 저장
+  const myVotes = useRef<boolean[][]>(
+    Array(vote).map((times) => Array(times).map(() => false)),
+  );
+  // 현재 진행중인 투표에서 드래그 이전의 투표 임시저장
+  const prevMyVotes = useRef<boolean[][]>(
+    Array(vote).map((times) => Array(times).map(() => false)),
+  );
+  // 현재 진행중인 투표에서 드래그 중인 영역 임시저장
+  const currentMyVotes = useRef<boolean[][]>(
+    Array(vote).map((times) => Array(times).map(() => false)),
+  );
+  // MyTimeTable 각 cell 2차원배열
+  const myCells = useRef<(HTMLDivElement | null)[][]>(
+    Array(vote).map((times) => Array(times).map(() => null)),
+  );
+  // VotedTimeTable 각 cell 2차원배열
+  const votedCells = useRef<(HTMLDivElement | null)[][]>(
+    Array(vote).map((times) => Array(times).map(() => null)),
+  );
+  // 이전 영역의 x,y 인덱스들을 문자열로 합친 배열
+  const prevRange = useRef<string[]>([]);
 
-  const checkSelectionRange = useCallback((target: HTMLDivElement) => {
-    const [startY, startX] = dragStartPoint.current;
-    const isSelected = prevVotes.current[startY][startX];
-    const endY = Number(target.dataset.col);
-    const endX = Number(target.dataset.row);
-    const minY = Math.min(startY, endY);
-    const maxY = Math.max(startY, endY);
-    const minX = Math.min(startX, endX);
-    const maxX = Math.max(startY, endY);
+  // 드래그 시작점의 투표 여부
+  const isVoted = useRef(false);
 
-    const isOver = (col: number, row: number) =>
-      col >= minY && col <= maxY && row >= minX && row <= maxX;
+  const onMouseMove = (event: MouseEvent) => {
+    if (isTargetContains(event, dragAreaRef.current) && voteAreaRef.current) {
+      const y = Number(event.target.dataset.col);
+      const x = Number(event.target.dataset.row);
 
-    const isStart = startY === endY && startX === endX;
+      // 선택 영역에 변경사항 없을시 얼리리턴
+      if (y === prevDragPoint.current[0] && x === prevDragPoint.current[1])
+        return;
 
-    return { isSelected, isStart, isOver };
-  }, []);
+      const [startY, startX] = startDragPoint.current;
 
-  const onMouseMove = useCallback((event: MouseEvent) => {
-    if (isTargetContains(event, dragAreaRef.current)) {
-      //FIXME: 콘솔
-      console.log('onMouseMove:', event.target);
+      const minY = Math.min(startY, y);
+      const maxY = Math.max(startY, y);
+      const minX = Math.min(startX, x);
+      const maxX = Math.max(startX, x);
 
-      const { isSelected, isStart, isOver } = checkSelectionRange(event.target);
+      let currentRange: string[] = [];
 
-      if (isStart) return;
-
-      const myTable = getChildArray(dragAreaRef.current?.childNodes).map(
-        (column) => getChildArray(column?.childNodes),
-      );
-      const voteTable = getChildArray(voteAreaRef.current?.childNodes).map(
-        (column) => getChildArray(column?.childNodes),
-      );
-
-      myTable.forEach((myColumn, i) =>
-        myColumn.forEach((myCell, j) => {
-          const voteCell = voteTable[i][j];
-
-          if (
-            myCell instanceof HTMLDivElement &&
-            voteCell instanceof HTMLDivElement
-          ) {
-            const col = Number(myCell.dataset.col);
-            const row = Number(myCell.dataset.row);
-
-            //FIXME: 콘솔
-            console.log(myCell);
-
-            myCell.classList.toggle('selected', isOver(col, row) && isSelected); // 내 투표테이블 색상 변경
-            voteCell.classList.toggle(
-              `voted-mine`,
-              isOver(col, row) && isSelected,
-            ); // 전체 투표테이블 색상 변경
+      // 현재 선택 영역의 배경색을 드래그 시작점의 투표 여부(isVoted)로 결정
+      for (let i = minY; i <= maxY; i++) {
+        for (let j = minX; j <= maxX; j++) {
+          currentRange = [...currentRange, '' + i + j];
+          if (isVoted.current) {
+            myCells.current[i][j]?.classList.add('selecting');
+            votedCells.current[i][j]?.classList.add('voting');
+          } else {
+            myCells.current[i][j]?.classList.add('unselecting');
+            votedCells.current[i][j]?.classList.add('unvoting');
           }
-        }),
-      );
-    }
-  }, []);
-
-  const onMouseDown = useCallback(
-    (event: MouseEvent) => {
-      if (event.target instanceof HTMLDivElement) {
-        //FIXME: 콘솔
-        console.log('onMouseDown:', event.target);
-        console.log('prevVotes:', prevVotes.current);
-
-        const y = Number(event.target.dataset.col);
-        const x = Number(event.target.dataset.row);
-
-        dragStartPoint.current = [y, x];
-
-        //FIXME: 콘솔
-        console.log('y:', y, 'x:', x);
-
-        prevVotes.current[y][x] = !prevVotes.current[y][x];
-
-        onMouseMove(event);
+          currentMyVotes.current[i][j] = isVoted.current;
+        }
       }
-    },
-    [onMouseMove],
-  );
 
-  const onMouseUp = useCallback((event: MouseEvent) => {
+      // 줄어든 영역 제거
+      prevRange.current
+        .filter((cellIndex) => !currentRange.includes(cellIndex))
+        .map((str): number[] => str.split('').map(Number))
+        .forEach(([diffY, diffX]) => {
+          myCells.current[diffY][diffX]?.classList.remove('selecting');
+          myCells.current[diffY][diffX]?.classList.remove('unselecting');
+          votedCells.current[diffY][diffX]?.classList.remove('voting');
+          votedCells.current[diffY][diffX]?.classList.remove('unvoting');
+          currentMyVotes.current[diffY][diffX] =
+            prevMyVotes.current[diffY][diffX];
+        });
+
+      // 이전 범위를 현재 범위로 교체
+      prevRange.current = [...currentRange];
+
+      prevDragPoint.current = [y, x];
+    }
+  };
+
+  const onMouseDown = (event: MouseEvent) => {
     if (isTargetContains(event, dragAreaRef.current)) {
-      //FIXME: 콘솔
-      console.log('onMouseUp:', event);
+      const y = Number(event.target.dataset.col);
+      const x = Number(event.target.dataset.row);
 
-      const { isSelected, isOver } = checkSelectionRange(event.target);
+      startDragPoint.current = [y, x];
 
-      prevVotes.current = prevVotes.current.map((col, i) =>
-        col.map((row, j) => {
-          return isOver(i, j) ? isSelected : row;
-        }),
-      );
+      prevRange.current = ['' + y + x];
+
+      isVoted.current = !prevMyVotes.current[y][x];
+
+      onMouseMove(event);
     }
-  }, []);
+  };
 
-  const onMouseLeave = useCallback(
-    (event: MouseEvent) => {
-      onMouseUp(event);
-    },
-    [onMouseUp],
-  );
+  const onMouseUp = (event: MouseEvent) => {
+    prevDragPoint.current = [-1, -1];
 
-  //TODO: prevVotes 투표 배열 초기화
+    if (isTargetContains(event, dragAreaRef.current)) {
+      prevMyVotes.current = [...currentMyVotes.current];
+    }
+
+    myCells.current.forEach((col, i) =>
+      col.forEach((cell, j) => {
+        cell?.classList.remove('selecting');
+        cell?.classList.remove('unselecting');
+        cell?.classList.toggle('selected', prevMyVotes.current[i][j]);
+        votedCells.current[i][j]?.classList.remove('voting');
+        votedCells.current[i][j]?.classList.remove('unvoting');
+        votedCells.current[i][j]?.classList.toggle(
+          'voted-mine',
+          prevMyVotes.current[i][j],
+        );
+      }),
+    );
+  };
+
   useEffect(() => {
-    const dates = Object.values(vote);
+    if (dragAreaRef.current && voteAreaRef.current) {
+      const dates = Object.values(vote);
 
-    //FIXME: 콘솔
-    console.log('dates:', dates);
+      // 데이터 구조상 행,열이 바뀌어있기 때문에 전치시켜야됨
+      myCells.current = getChildArray<HTMLDivElement>(
+        dragAreaRef.current?.childNodes,
+      ).map((column) => getChildArray(column.childNodes));
 
-    const dragArray = getChildArray(dragAreaRef.current?.childNodes).map(
-      (column) => getChildArray(column.childNodes),
-    );
+      votedCells.current = getChildArray<HTMLDivElement>(
+        voteAreaRef.current?.childNodes,
+      ).map((column) => getChildArray(column.childNodes));
 
-    const voteArray = getChildArray(voteAreaRef.current?.childNodes).map(
-      (column) => getChildArray(column.childNodes),
-    );
+      const newVotes = Array.from(dates, (times) =>
+        Array(times).map(() => false),
+      );
 
-    //FIXME: 콘솔
-    console.log(voteArray);
+      dates.forEach((timeMap, i) => {
+        const times = Object.values(timeMap);
 
-    dates.forEach((timeMap, i) => {
-      const times = Object.values(timeMap);
-      if (!prevVotes.current[i]) {
-        prevVotes.current[i] = [];
-      }
-      times.forEach((users, j) => {
-        prevVotes.current[i][j] = users.some(({ id }) => userId === id);
+        // 두 타임테이블을 받아온 데이터로 초기화
+        times.forEach((users, j) => {
+          const myNode = myCells.current[i][j];
+          const voteNode = votedCells.current[i][j];
 
-        const myNode = dragArray[i][j];
+          // 현재 투표용 newVotes 초기화
+          newVotes[i][j] = users.some(({ id }) => userId === id);
 
-        if (myNode instanceof HTMLDivElement) {
-          myNode.classList.toggle('selected', prevVotes.current[i][j]);
-        }
+          // 이전에 투표한 곳이면 색상 변경
+          myNode?.classList.toggle('selected', newVotes[i][j]);
 
-        const voteNode = voteArray[i][j];
-
-        if (voteNode instanceof HTMLDivElement) {
+          // 투표 인원수가 많을수록 채도 상승
           const voteCount = users.filter(({ id }) => userId !== id).length;
-          voteNode.classList.add(`voted-${voteCount}`);
-        }
+          voteNode?.classList.add(`voted-${voteCount}`);
+          voteNode?.classList.toggle(`voted-mine`, newVotes[i][j]);
+        });
       });
-    });
+
+      myVotes.current = transpose(newVotes);
+      currentMyVotes.current = transpose(newVotes);
+      prevMyVotes.current = transpose(newVotes);
+      myCells.current = transpose(myCells.current);
+      votedCells.current = transpose(votedCells.current);
+    }
   }, []);
 
   const { dragAreaRef } = useDragArea({
     onMouseDown,
     onMouseMove,
     onMouseUp,
-    onMouseLeave,
   });
 
   return { dragAreaRef, voteAreaRef };
@@ -181,8 +195,29 @@ const isTargetContains = (
   );
 };
 
-const getChildArray = (nodes: NodeListOf<ChildNode> | undefined) => {
-  const result: ChildNode[] = [];
-  nodes?.forEach((el) => result.push(el));
+/**
+ * 인수로 받은 자식 요소들을 배열에 담아 반환합니다.
+ * ChildNode 타입은 일반적인 배열이 아니기 때문에 배열 메소드를 쓰기 위해 필요했습니다.
+ *
+ * @param nodes DOM 요소의 ChildNode NodeList
+ * @returns 제네릭으로 받은 타입의 배열 반환
+ */
+const getChildArray = <T>(nodes: NodeListOf<ChildNode> | undefined) => {
+  const result: T[] = [];
+  if (nodes == null) return [];
+  nodes?.forEach((el) => {
+    result.push(el as T);
+  });
   return result;
+};
+
+/**
+ * 2차원 배열의 x축과 y축을 교체하는 배열 전치 함수입니다.
+ * useVotingTimeTable에서는 props로 넘어오는 vote가 행,열이 바뀌어 있어서 정제하는 과정이 필요했습니다.
+ *
+ * @param array 2차원 배열
+ * @returns 전치된 배열
+ */
+const transpose = <T>(array: T[][]) => {
+  return array[0].map((_, colIndex) => array.map((row) => row[colIndex]));
 };
