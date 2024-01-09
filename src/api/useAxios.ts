0dbae36dffd441c355cb55e94209eec5f2d5a21d
wdgWon <same666@naver.com>
2024-01-10 00:20:45 +0900

    Fix : useAxios 변경 사항 추가

--- src/api/useAxios.ts
+++ src/api/useAxios.ts
@@ -4,31 +4,31 @@ import { AxiosError, AxiosResponse, isAxiosError } from 'axios';
 const useAxios = <T>(
   cbFn: () => Promise<AxiosResponse<T, unknown>>,
   deps?: string,
+  fetchOnMount = false,
 ) => {
   const [response, setResponse] = useState<T>({} as T);
   const [error, setError] = useState<Error | AxiosError | null>(null); //
   const [isLoading, setIsLoading] = useState(true);
-  // const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const res = await cbFn();
+      if (!isAxiosError(res)) setResponse(res.data);
+      return res;
+    } catch (err) {
+      setError(err as Error | AxiosError | null);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const res = await cbFn();
-        if (!isAxiosError(res)) setResponse(res.data);
-        return res;
-      } catch (err) {
-        setError(err as Error | AxiosError | null);
-        // setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    void fetchData();
+    if (fetchOnMount) void fetchData();
   }, [deps]);
 
-  return { response, error, isLoading };
+  return { response, fetchData, error, isLoading };
 };
 
 export default useAxios;
