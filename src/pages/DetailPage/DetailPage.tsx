9475a1c8343a2da968ec6b229f9d705be0a765e8
타래 <85001878+meowTarae@users.noreply.github.com>
2024-01-10 21:02:46 +0900

    Chore: 주석추가

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,6 +5,8 @@ import { DetailComment } from './DetailComment/DetailComment';
 import { DetailMeetDescription } from './DetailMeetDescription';
 import { DetailPost } from './DetailPost/DetailPost';
 import { DetailTab } from './DetailTab';
+import { useSelector } from '@/_redux/hooks';
+import { RootStateType } from '@/_redux/store';
 import { IComment, IPost } from '@/api/_types/apiModels';
 import { getApi } from '@/api/apis';
 import useAxios from '@/api/useAxios';
@@ -24,11 +26,16 @@ export const DetailPage = () => {
     // getApi('posts/${_id}');
     getApi('posts/6597691a888bed1583ee96f8'),
   );
+  // 하트 아이콘 클릭 시 좋아요 올라가는지 체크
+  // 이를 위해 redux를 적용시켜야 한다.
+  const isLogin = useSelector((state: RootStateType) => state.auth.isLogin);
+  useEffect(() => {
+    console.log('로그인 상태:', isLogin);
+  }, [isLogin]);
 
   // PR : 1. 아래 useEffect에서 디펜던시 어레이를 넣어야 하는지, 혹은 수정되어야 할 부분은 없는지..
   //      2. 리팩토링 해야하는 부분은 없는지.
   //      3. 코드에 as 없애고 싶은데 어케 없애야 하나요 ;ㅅ;
-  //      5. 4번때매 badge 컴포넌트에 어떤 정보를 보내야할지도 모르겠음.
   useEffect(() => {
     const handleAPIError = () => {
       alert('API로부터 데이터를 받아올 때 에러가 발생했습니다.');
