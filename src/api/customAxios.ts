fc9963136ff0f50781510b6ce3b9c91ad9378a57
yejinleee <yejin9487@gmail.com>
2024-01-10 13:07:22 +0900

    Chore: 테스트용 VITE_TEST_TOKEN 환경변수 추가

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,21 +1,23 @@
-import { getItem } from '@/utils/storage';
+// import { getItem } from '@/utils/storage';
 import axios, { AxiosInstance } from 'axios';
 
 const { VITE_API_BASE_URL } = import.meta.env;
+const { VITE_TEST_TOKEN } = import.meta.env;
 
 export const customAxios = (): AxiosInstance => {
   const axiosInstance = axios.create({
-    baseURL: VITE_API_BASE_URL as string,
+    baseURL: VITE_API_BASE_URL,
   });
 
   return axiosInstance;
 };
 export const customAxiosJWT = (): AxiosInstance => {
   // const jwt = getItem('JWT')로 받아와야함.
-  const jwt = '123456';
   const axiosInstance = axios.create({
-    baseURL: VITE_API_BASE_URL as string,
-    headers: { Authorization: `Bearer ${jwt}` },
+    baseURL: VITE_API_BASE_URL,
+    headers: {
+      Authorization: VITE_TEST_TOKEN,
+    },
   });
 
   return axiosInstance;
