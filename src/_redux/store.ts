0b807e2c87b751165da15d4c7b235f417d7c2176
yejinleee <yejin9487@gmail.com>
2024-01-11 09:44:40 +0900

    Feat: 오늘날짜 슬라이스 추가 및 이날모일래 날짜 기준 오늘로 수정

--- src/_redux/store.ts
+++ src/_redux/store.ts
@@ -7,6 +7,7 @@ import { getAuthorPostSlice } from './slices/postSlices/getAuthorPostSlice';
 import { getChannelPostsSlice } from './slices/postSlices/getChannelPostSlice';
 import { getPostDetailSlice } from './slices/postSlices/getPostSlice';
 import { putPostSlice } from './slices/postSlices/putPostSlice';
+import todaySlice from './slices/todaySlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
@@ -20,6 +21,7 @@ export const store = configureStore({
     getChannelPost: getChannelPostsSlice,
     getPostDetail: getPostDetailSlice,
     putPost: putPostSlice,
+    today: todaySlice,
   },
 });
 
