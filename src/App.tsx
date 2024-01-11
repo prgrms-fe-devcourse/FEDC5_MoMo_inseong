6b74f64c78cd26b374f7637fac2eafe4851e086f
yejinleee <yejin9487@gmail.com>
2024-01-12 01:20:24 +0900

    Feat: redux-persist 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import { Outlet } from 'react-router-dom';
 import { store } from './_redux/store';
 import { GlobalReset } from './style/GlobalReset';
 import { theme } from './style/theme';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+export const persistor = persistStore(store);
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalReset />
-        <Outlet />
-      </ThemeProvider>
+      <PersistGate
+        loading={null}
+        persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <GlobalReset />
+          <Outlet />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 };
