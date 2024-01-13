ff406041ec675800f2518c0d3f525ba529007b7a
yejinleee <yejin9487@gmail.com>
2024-01-13 23:36:19 +0900

    Chord: 컴포넌트 임시 index.ts  추가

---
+++ src/components/_common/index.ts
@@ -0,0 +1,14 @@
+import { Button } from './Button/Button';
+import { Card } from './Card/Card';
+import { Comment } from './Comment/Comment';
+import { Header } from './Header/Header';
+import { Icon } from './Icon/Icon';
+import { Profile } from './Profile/Profile';
+import { Tab } from './Tab/Tab';
+import { Tag } from './Tag/Tag';
+import { Tooltip } from './Tooltip/Tooltip';
+
+export { Button, Card, Comment, Header, Icon, Profile, Tab, Tag, Tooltip };
+
+// TODO: Input 추가
+// TODO: 각 파일에서 여기 경로로부터 import
